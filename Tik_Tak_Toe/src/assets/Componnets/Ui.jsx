1607b22bf6154b7ab734aed5d1571dfc6ea3c2a3
rajan <rajandhamala0123@gmail.com>
2024-08-28 14:50:16 +0545

    added simple player turn ui

--- Tik_Tak_Toe/src/assets/Componnets/Ui.jsx
+++ Tik_Tak_Toe/src/assets/Componnets/Ui.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function Ui(){
 
-    function handelClick(position){
-        console.log('button clicked at',position)
+    function handelreload(){
+        window.location.reload()
     }
 
-    const position=[
+
+    const [board,Setboard]=useState(Array(9).fill(null))
+    const [isNext,SetisNext]=useState(true)
+
+
+    function handelClick(position){
+
+        const newBoard=board.slice()
+
+        if(newBoard[position]){
+            alert('place is already occupied')
+        }
+        newBoard[position]=isNext ? 'X' :'0'
+        Setboard(newBoard)
+        SetisNext(!isNext)
+        
+    }
+
+    const grid=[
         [0,1,2],
         [3,4,5],
         [6,7,8]
@@ -14,7 +32,7 @@ function Ui(){
     return(
         <>
         <div className=' h-[100vh] flex flex-col justify-center items-center'>
-        {position.map((row, rowIndex) => (
+        {grid.map((row, rowIndex) => (
                 <div 
                     key={rowIndex}
                     className={`flex`}
@@ -22,14 +40,22 @@ function Ui(){
                     {row.map(position => (
                         <button
                             key={position}
-                            className='h-20 w-20 border border-black'
+                            className='h-20 w-20 border text-2xl font-bold border-black'
                             onClick={() => handelClick(position)}
-                        >
+                        >{board[position]}
                         </button>
                     ))}
                 </div>
             ))}
+              <div className='mt-5 text-lg'>
+                        NextPlayer:{isNext ? '1' : '2'}
+
+                    </div>
+                    <button className='border rounded px-2' onClick={()=>{
+                        handelreload()
+                    }}>Reload</button>
         </div>
+        
         </>
     )
 }
