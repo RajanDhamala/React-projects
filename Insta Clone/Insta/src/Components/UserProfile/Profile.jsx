acf7a217e456d711842080bb07a4c332a4fe4d6a
rajan <rajandhamala0123@gmail.com>
2024-09-09 17:10:06 +0545

    making user profile component

--- Insta Clone/Insta/src/Components/UserProfile/Profile.jsx
+++ Insta Clone/Insta/src/Components/UserProfile/Profile.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-function Profile() {
+function Profile({username='niamhhendx',}) {
   return (
-    <div>Profile</div>
+    <>
+    <div>
+        <div className='w-full border-y border-gray-300 md:hidden flex flex-row justify-between items-center'>
+        <div className='flex ml-5 items-center'>
+        <button className='font-semibold text-3xl'>{'<'}</button>
+        </div>
+        <div className='flex justify-start w-72 items-center'>
+            <h1 className='py-1.5 text-center font-semibold'>{username}</h1>
+            </div>
+            
+        </div>
+    </div>
+    </>
   )
 }
 
