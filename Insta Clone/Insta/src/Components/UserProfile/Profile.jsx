9e9aeac8581e307a8d84bd383ede48ca2ed9a222
rajan <rajandhamala0123@gmail.com>
2024-09-10 19:14:52 +0545

    using better approch for responsiveness

--- Insta Clone/Insta/src/Components/UserProfile/Profile.jsx
+++ Insta Clone/Insta/src/Components/UserProfile/Profile.jsx
@@ -7,7 +7,6 @@ function Profile({ username = 'niamhhendx', img = 'https://img.freepik.com/free-
  const menu = useRef(null)
   return (
     <>
-      <div>
         <div className='w-full border-y border-gray-300 md:hidden flex flex-row justify-between items-center'>
           <div className='flex ml-5 items-center'>
             <button className='text-3xl h-10'>{'<'}</button>
@@ -16,43 +15,49 @@ function Profile({ username = 'niamhhendx', img = 'https://img.freepik.com/free-
             <h1 className='py-1.5 text-center font-semibold'>{username}</h1>
           </div>
         </div>
-      </div>
 
-      <section className='border-b-2'>
-        <div className='grid grid-cols-5 place-content-center border-gray-400 sm:grid-cols-2'>
-          <div className='md:w-full md:flex md:justify-end ml-3 md:ml-0'>
-          <div className="h-24 w-24  md:h-40 md:w-40 flex justify-start overflow-hidden rounded-full">
+      <section className='border-b-2 mt-5'>
+       <div className='flex md:justify-between  '>
+        <div className='flex md:justify-center w-full pl-3 gap-3'>
+        <div className="h-24 w-24  md:h-40 md:w-40 flex justify-start overflow-hidden rounded-full">
             <img
               src={img}
               alt="user image"
               className="object-cover w-full h-full"
             />
           </div>
-          </div>
-          <div className='flex ml-5 w-full justify-start'>
-            <div className='flex pl-2 pb-2 md:flex-row flex-col gap-2 md:gap-4'>
-              <div className='flex md:gap-4'>
-              <h1 className='text-2xl mr-2'>{username}</h1>  
-              <button className='font-bold text-2xl md:order-last flex justify-start' onClick={(e)=>{
+          <div className='grid grid-cols-2 md:flex md:gap-3'>
+          <h1 className='text-2xl mr-2'>{username}</h1>
+          <button className='font-bold text-2xl md:order-last flex justify-start col-start-2 h-7 md:block' onClick={(e)=>{
                  menu.current.classList.toggle('hidden');
               }}>...</button>
-              </div>
-          
-              <button className='text-white font-semibold cursor-pointer order-last hover:bg-blue-600 bg-blue-500 rounded-md h-7 w-40 md:w-20'>Follow</button>
+                <button className='text-white font-semibold cursor-pointer order-last hover:bg-blue-600 bg-blue-500 rounded-md h-7 col-span-2 md:w-20 md:order-2'>Follow</button>
+                <div className='md:flex  gap-5 md:absolute md:mt-14 items-center py-3 order-last hidden'>
+            <div className='text-center'>
+              <p>0</p>
+              <p className='text-gray-500'>posts</p>
+            </div>
+            <div className='text-center'>
+              <p>17</p>
+              <p className='text-gray-500'>followers</p>
+            </div>
+            <div className='text-center'>
+              <p>23</p>
+              <p className='text-gray-500'>following</p>
             </div>
           </div>
-          <div>
-
-          </div>
-        </div>
-        <div className='ml-3 mb-4 '>
+          <div className='ml-3 mb-4 '>
           <h1 className='mt-4 font-semibold mb-2 sm:text-center'>{username}</h1>
-          <div className=' flex items-center gap-1 font-semibold bg-red-500'>
+          <div className=' flex items-center gap-1 font-semibold '>
           <p className='text-sm text-gray-700 opacity-80 sm:text-center'>Followed by</p>
           <p className='text-sm cursor-pointer '>{followers}</p>
           </div>
+          </div>
+          </div>
         </div>
-        <div className='border-t border-gray-300 '>
+
+       </div>
+        <div className='border-t border-gray-300 md:hidden '>
           <div className='flex justify-evenly items-center py-3'>
             <div className='text-center'>
               <p>0</p>
@@ -70,7 +75,7 @@ function Profile({ username = 'niamhhendx', img = 'https://img.freepik.com/free-
         </div>
       </section>
       <div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center md:mt-2 '>
           <img src="https://media.istockphoto.com/id/936681148/vector/lock-icon.jpg?s=612x612&w=0&k=20&c=_0AmWrBagdcee-KDhBUfLawC7Gh8CNPLWls73lKaNVA=" alt="" className='w-14 h-14' />
           <div>
           <span>This account is private</span>
