2f10e18eb9894501c600d510a3c179d9a516aaba
rajan <rajandhamala0123@gmail.com>
2024-09-19 10:55:41 +0545

    update on feed

--- Insta Clone/Insta/src/Components/Feeds/Feed.jsx
+++ Insta Clone/Insta/src/Components/Feeds/Feed.jsx
@@ -7,22 +7,34 @@ function Feed() {
     <>
     <div>
         <div className='flex justify-between items-center'>
-            <h1 className=''>Instagram</h1>
+          <div className='flex items-center'>
+            <h1 className='italic font-[cursive] text-2xl font-semibold'>Instagram</h1>
+            <button className='rotate-180 text-[20px] ml-1 text-gray-500'>^</button>
+            </div>
             <div className='flex items-center justify-end gap-x-12 mr-5'>
             <div>
-                <button className='bg-gray-200 px-0.5 py-1'>🔍</button>
-                <input type="search" placeholder='Search' className='bg-gray-200 focus:outline-none py-1 px-1' />
+                <button className='bg-gray-200 py-1 absolute search'>🔍</button>
+                <i class="fal fa-search"></i>
+                <input type="search" placeholder='Search' className='bg-gray-200 focus:outline-none py-1 pl-6' onClick={(e)=>document.querySelector('.search').classList.toggle('hidden')} />
             </div>
             <button className='text-4xl text-black' onClick={()=>setisclicked(!isclicked)}>{
             isclicked ? '♥':'♡'}</button>
             </div>
+            
         </div>
+        <div className='w-full border-t-2 mb-4 mt-2 bg-gray-200'>
+
+            </div>
     </div>
-    <div className='flex'>
+    <div className='flex overflow-x-hidden mx-3'>
                 <Storylogo/>
+                <Storylogo imgsrc='https://r2.starryai.com/results/240758348/61b45728-2ff1-4197-aef8-b1d372e453b4.webp' username='Starr_yai' visiblty={true}/>
+                <Storylogo imgsrc='https://r2.starryai.com/results/1010278294/420e067f-8eaf-4229-af2d-fd6630cda10a.webp' username='Barry_hales' visiblty={true}/>
+                <Storylogo imgsrc='https://xsgames.co/randomusers/assets/avatars/male/46.jpg' username='Richard_haster' visiblty={false}/>
                 <Storylogo/>
-                <Storylogo/>
-                <Storylogo/>
+                <Storylogo imgsrc='https://r2.starryai.com/results/240758348/61b45728-2ff1-4197-aef8-b1d372e453b4.webp' username='Starr_yai' visiblty={true}/>
+                <Storylogo imgsrc='https://r2.starryai.com/results/1010278294/420e067f-8eaf-4229-af2d-fd6630cda10a.webp' username='Barry_hales' visiblty={true}/>
+                <Storylogo imgsrc='https://xsgames.co/randomusers/assets/avatars/male/46.jpg' username='Richard_haster' visiblty={false}/>
                 
     </div>
     </>
--- Insta Clone/Insta/src/Components/Stories/Storylogo.jsx
+++ Insta Clone/Insta/src/Components/Stories/Storylogo.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
-function Storylogo({ imgsrc='https://img.freepik.com/free-photo/selfie-portrait-videocall_23-2149186104.jpg',username='tinku_bahdur' }) {
+function Storylogo({
+  imgsrc = 'https://img.freepik.com/free-photo/selfie-portrait-videocall_23-2149186104.jpg',
+  username = 'tinku_bahdur',
+  visiblty=false,
+}) {
+  const [wasseen, setwasseen] = useState(visiblty);
+
+useEffect(()=>{
+  setwasseen(visiblty)
+},[visiblty])
+
   return (
     <>
-    <div  className='px-2'>
-    <div className="flex justify-center items-center">
-      <div className='h-20 w-20 rounded-full border-2 border-gray-200 flex justify-center items-center'>
-        <div className='h-16 w-16 rounded-full overflow-hidden'>
-          <img src={imgsrc} alt="story" className='w-full h-full object-cover' />
+      <div className='px-2'>
+        <div className='flex justify-center items-center'>
+          <div
+            className={`px-0.5 py-0.5 
+               rounded-full border-[3px] flex justify-center items-center ${
+              wasseen ? 'bg-gradient-to-bl from-pink-500  to-yellow-400 border-0' : ''
+            }`}
+          >
+            <div className='h-16 w-16 rounded-full overflow-hidden'>
+              <img src={imgsrc} alt='story' className='w-full h-full object-cover' />
+            </div>
+          </div>
         </div>
+        <h2 className='text-sm text-gray-700 cursor-pointer'>{username}</h2>
       </div>
-    </div> 
-    <h2 className='text-sm text-gray-700 cursor-pointer'>{username}</h2>
-    </div>
     </>
   );
 }
